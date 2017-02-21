be36fea8c61066e8efe46ee912e2d177ec2cf748
ValentinZhao <2264709714@qq.com>
2017-02-21 23:49:55 +0800

    select sort

--- src/Sort/SelectSort.java
+++ src/Sort/SelectSort.java
@@ -20,5 +20,25 @@ public class SelectSort {
 				System.out.print(a[i] + " ");
 			}
 		}
-	}	
+	}
+	
+	public static int[] selectSort(int[] arr){
+		if(arr == null || arr.length == 0){
+			return null;
+		}
+		int len = arr.length;
+		for(int i = 0, k = 0; i < len; i++, k = i){
+			for(int j = i + 1; j < len; j++){
+				if(arr[k] > arr[j]){//后面有更小的值,这个for结束之后选一个最小的值
+					k = j;
+				}
+			}
+			if(i != k){
+				int temp = arr[i];
+				arr[i] = arr[k];
+				arr[k] = temp;
+			}
+		}
+		return arr;
+	}
 }
