b90458660c4ad476c050683175a119434bfdce8e
ValentinZhao <2264709714@qq.com>
2017-03-08 17:45:02 +0800

    interview 8

--- src/CodingInterviews/Interview8.java
+++ src/CodingInterviews/Interview8.java
@@ -1,5 +1,39 @@
 package CodingInterviews;
 
+/**
+ * 旋转数组的最小数字
+ *
+ */
 public class Interview8 {
+	public int min(int[] nums, int length){
+		int preIndex = 0;
+		int rearIndex = length - 1;
+		int midIndex = 0;
+		while(nums[preIndex] >= nums[rearIndex]){
+			if(rearIndex - preIndex == 1){
+				midIndex = rearIndex;
+				break;
+			}
+			midIndex = (preIndex + rearIndex) / 2;
+			if(nums[preIndex] == nums[rearIndex] && nums[preIndex] == nums[midIndex]){
+				return minByOrder(nums, preIndex, rearIndex);
+			}
+			if(nums[preIndex] <= nums[midIndex]){
+				preIndex = midIndex;
+			} else if(nums[rearIndex] >= nums[midIndex]) {
+				rearIndex = midIndex;
+			}
+		}
+		return nums[midIndex];
+	}
 
+	private int minByOrder(int[] nums, int preIndex, int rearIndex) {
+		int result = nums[preIndex];
+		for(int i = preIndex + 1; i <= rearIndex; ++i){
+			if(result > nums[i]){
+				result = nums[i];
+			}
+		}
+		return result;
+	}
 }
