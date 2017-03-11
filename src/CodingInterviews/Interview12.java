f4c679179f9e71cd01dfa8605bd0f8ac88581d8a
ValentinZhao <2264709714@qq.com>
2017-03-11 17:54:12 +0800

    interview12

---
+++ src/CodingInterviews/Interview12.java
@@ -0,0 +1,45 @@
+package CodingInterviews;
+
+/**
+ * 打印1到最大的n位数
+ *
+ */
+public class Interview12 {
+	
+	public static void print1toMaxOfDigits(int n){
+		if(n <= 0){
+			return;
+		}
+		char[] number = new char[n + 1];
+		number[n] = '\0';
+		for(int i = 0; i < 10; ++i){
+			number[0] = (char) i;
+			print1toMaxOfDigitsRecursively(number, n, 0);
+		}
+		number = null;
+	}
+
+	private static void print1toMaxOfDigitsRecursively(char[] number, int n, int index) {
+		if(index == n - 1){
+			printNumber(number);
+			return;
+		}
+		for(int i = 0; i < 10; ++i){
+			number[index + 1] = (char) i;
+			print1toMaxOfDigitsRecursively(number, n, index + 1);
+		}
+	}
+
+	private static void printNumber(char[] number) {
+		boolean isInitWith0 = true;
+		int length = number.length;
+		for(int i = 0; i < length; ++i){
+			if(isInitWith0 && number[i] != '0'){
+				isInitWith0 = false;
+			}
+			if(!isInitWith0){
+				System.out.print(number[i]);
+			}
+		}
+	}
+}
