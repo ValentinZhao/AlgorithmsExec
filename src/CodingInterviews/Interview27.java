cbb25fe6d75a83310b8566285bbec625f98e601a
ValentinZhao <2264709714@qq.com>
2017-03-12 17:29:30 +0800

    interview27

---
+++ src/CodingInterviews/Interview27.java
@@ -0,0 +1,37 @@
+package CodingInterviews;
+/**
+ * 二叉搜索树与双向链表
+ * @author zhaoziliang
+ *
+ */
+public class Interview27 {
+	static class BinaryTreeNode{
+		int value;
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+	}
+	
+	public static BinaryTreeNode convert(BinaryTreeNode head){
+		if(head == null){
+			return null;
+		}
+		if(head.left == null && head.right == null){
+			return head;
+		}
+		BinaryTreeNode left = convert(head.left);
+		BinaryTreeNode p = left;
+		while(p != null && p.right != null){
+			p = p.right;
+		}
+		if(left != null){
+			p.right = head;
+			head.left = p;
+		}
+		BinaryTreeNode right = convert(head.right);
+		if(right != null){
+			right.left = head;
+			head.right = right;
+		}
+		return left != null ? left : head;
+	}
+}
