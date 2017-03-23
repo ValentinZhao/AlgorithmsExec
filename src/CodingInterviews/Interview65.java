9ac5e1fd761b3d0372b1f1f25d2929bde6f08946
ValentinZhao <2264709714@qq.com>
2017-03-23 14:49:30 +0800

    interview65

--- src/CodingInterviews/Interview65.java
+++ src/CodingInterviews/Interview65.java
@@ -1,5 +1,39 @@
 package CodingInterviews;
 
-public class Interview65 {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+/**
+ * 滑动窗口中最大的值
+ * @author zhaoziliang
+ *
+ */
+public class Interview65 {
+	public ArrayList<Integer> maxInWindows(int[] num, int size){
+		ArrayList<Integer> res = new ArrayList<>();
+		if(num == null){
+			return res;
+		}
+		if(num.length < size || size < 1){
+			return res;
+		}
+		LinkedList<Integer> indexDeque = new LinkedList<>();
+		for(int i = 0; i < size - 1; i--){
+			while(!indexDeque.isEmpty() && num[indexDeque.getLast()] < num[i]){
+				indexDeque.removeLast();
+			}
+			indexDeque.addLast(num[i]);
+		}
+		for(int i = size - 1; i < num.length; i++){
+			while(!indexDeque.isEmpty() && num[indexDeque.getLast()] < num[i]){
+				indexDeque.removeLast();
+			}
+			indexDeque.addLast(num[i]);
+			if(i - indexDeque.getFirst() + 1 > size){
+				indexDeque.removeFirst();
+			}
+			res.add(num[indexDeque.getFirst()]);
+		}
+		return res;
+	}
 }
