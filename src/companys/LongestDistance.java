3c507e859780b309fd70314c2a1775a00b1b5c4e
ValentinZhao <2264709714@qq.com>
2017-03-25 20:23:27 +0800

    distance

---
+++ src/companys/LongestDistance.java
@@ -0,0 +1,19 @@
+package companys;
+
+public class LongestDistance {
+	public int getDis(int[] A, int n) {
+		int dis = 0;
+		if(n > 1){
+			int min = A[0];
+			for(int i = 1; i < n; i++){
+				if(A[i] - min > dis){
+					dis = A[i] = min;
+				}
+				if(min > A[i]){
+					min = A[i];
+				}
+			}
+		}
+		return dis;
+    }
+}
