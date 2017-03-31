7d039c00786aa636c47f4d048c4a2b69c21a1f75
ValentinZhao <2264709714@qq.com>
2017-03-31 13:43:32 +0800

    max gap

---
+++ src/companys/MaxGap.java
@@ -0,0 +1,24 @@
+package companys;
+
+public class MaxGap {
+	public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = in.nextInt();
+            }
+            int maxFull = Integer.MIN_VALUE;
+            int minMaxGap = Integer.MAX_VALUE;
+            for(int i = 1; i < n; i++){
+                maxFull = Math.max(maxFull, arr[i] - arr[i - 1]);
+            }
+            for(int i = 1; i < n - 1; i++){
+                minMaxGap = Math.min(minMaxGap, Math.max(arr[i+1]-arr[i-1], maxFull));
+            }
+            System.out.println(minMaxGap);
+        }
+        in.close();
+    }
+}
