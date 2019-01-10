85bb19d96de3db4456637601974bd2060e58ef44
ValentinZhao <2264709714@qq.com>
2019-01-10 13:54:38 +0800

    add lc112

---
+++ src/leetcode/lc112/Solution112.java
@@ -0,0 +1,15 @@
+package lc112;
+
+class Solution112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return sum == root.val;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+}
