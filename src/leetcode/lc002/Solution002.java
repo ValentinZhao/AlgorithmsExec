d05090500dd7bba013cbd90e2c5d68352f9a7463
ValentinZhao <2264709714@qq.com>
2019-01-14 22:56:15 +0800

    add lc002 new comments

--- src/leetcode/lc002/Solution002.java
+++ src/leetcode/lc002/Solution002.java
@@ -25,7 +25,8 @@ public class Solution002 {
 			// 不同点在于，由于下下一行runner的引用地址换到了runner.next，而preHead没动，这就导致下一次循环到这里时
 			// preHead的地址一直没变过，而runner跑到了preHead的next节点，再下一次循环由于runner.next再次更新，preHead.next.next就有了新的值
 			// 同时这个preHead.next.next，也就是本循环的runner.next再次把地址给到runner，相当于再次把preHead.next.next和runner的地址同步了起来
-			// 所谓同步地址，其实就是runner把指针指向了runner.next，然后刚才runner.next也被preHead.next指向着，这样一退就把地址更新了
+			// 也就是说runner.next保存的地址一直是preHead的尾next的地址，每次runner.next被赋值，preHead就延长了
+			// runner = runner.next很明显就是把runner往后推一位，其实就是把runner在preHead上向后推一位（毕竟一开始起点是一样的）
 			// 它的作用就很明显了，把runner作为preHead的"奔跑"节点，一直在链表尾
 			runner.next = new ListNode(carry % 10);
 			runner = runner.next;
