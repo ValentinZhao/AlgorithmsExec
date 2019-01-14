cd8975079f4d3216e28f3ad9e48361b34746e38c
ValentinZhao <2264709714@qq.com>
2019-01-14 23:19:43 +0800

    add lc003

---
+++ src/leetcode/lc003/Solution003.java
@@ -0,0 +1,21 @@
+package lc003;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 滑动窗口题！！Sliding Window
+public class Solution003 {
+	public int lengthOfLongestSubstring(String s) {
+		Queue<Character> queue = new LinkedList<>();
+		int res = 0;
+		for (char c : s.toCharArray()) {
+			// 这里保证了，重复的字符，和它之前的部分都会被推出队列，留下的部分一定不会和当前字符重复
+			while (queue.contains(c)) {
+				queue.poll();
+			}
+			queue.offer(c); // 把这个字符补在后面
+			res = Math.max(res, queue.size());
+		}
+		return res;
+	}
+}
