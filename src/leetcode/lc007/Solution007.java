fcfcc4f04021612005e6bf11a00fc544ee20d199
ValentinZhao <2264709714@qq.com>
2019-01-18 18:53:29 +0800

    update lc007

--- src/leetcode/lc007/Solution007.java
+++ src/leetcode/lc007/Solution007.java
@@ -4,8 +4,8 @@ public class Solution007 {
     public int reverse(int x) {
         int sum = 0;
         while (x != 0) {
-            if ((sum > Integer.MAX_VALUE || sum == Integer.MAX_VALUE && x % 10 > 7) ||
-                    (sum < Integer.MIN_VALUE || sum == Integer.MIN_VALUE && x % 10 > -8)) {
+            if ((sum > Integer.MAX_VALUE / 10|| sum == Integer.MAX_VALUE / 10 && x % 10 > 7) ||
+                    (sum < Integer.MIN_VALUE / 10 || sum == Integer.MIN_VALUE / 10 && x % 10 < -8)) {
                 return 0;
             }
             sum = sum * 10 + x % 10;
