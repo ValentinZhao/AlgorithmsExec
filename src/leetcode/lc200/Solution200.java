6fb87057df0834d703c55c0a50ba0f7e77690cc7
ValentinZhao <2264709714@qq.com>
2019-01-20 17:48:03 +0800

    add lc200

---
+++ src/leetcode/lc200/Solution200.java
@@ -0,0 +1,30 @@
+package lc200;
+
+public class Solution200 {
+    private int row = 0;
+    private int col = 0;
+    private int count = 0;
+    public int numIslands(char[][] grid) {
+        row = grid.length;
+        if (row == 0) return 0;
+        col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= row || j >= col || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+}
