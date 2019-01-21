a7cbe117ceecd3adfa901116c9c6077917474b15
ValentinZhao <2264709714@qq.com>
2019-01-21 22:12:43 +0800

    add lc015

---
+++ src/leetcode/lc015/Solution015.java
@@ -0,0 +1,27 @@
+package lc015;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution015 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1, end = nums.length - 1, sum = 0 - nums[i];
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+                while (start < end) {
+                    if (nums[start] + nums[end] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        while (start < end && nums[start] == nums[start + 1]) start++;
+                        while (start < end && nums[end] == nums[end - 1]) end--;
+                        start++;end--;
+                    } else if (nums[start] + nums[end] > sum) end--;
+                    else start++;
+                }
+            }
+        }
+        return res;
+    }
+}
