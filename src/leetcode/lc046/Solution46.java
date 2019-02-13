3703009f99d3651a29e9f34596a58aa02a2ffff2
ValentinZhao <2264709714@qq.com>
2019-02-13 18:05:19 +0800

    add lc046

---
+++ src/leetcode/lc046/Solution46.java
@@ -0,0 +1,25 @@
+package lc046;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution046{
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtracking(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void backtracking(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+                backtracking(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
