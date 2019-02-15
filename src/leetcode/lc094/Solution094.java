b852f4c3494e25cb4d30df6123b37c225572084a
ValentinZhao <2264709714@qq.com>
2019-02-15 16:22:09 +0800

    add lc094 lc098 lc230

---
+++ src/leetcode/lc094/Solution094.java
@@ -0,0 +1,24 @@
+package lc094;
+
+import TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution094 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+        return list;
+    }
+}
---
+++ src/leetcode/lc098/Solution098.java
@@ -0,0 +1,26 @@
+package lc098;
+
+import TreeNode.TreeNode;
+
+import java.util.Stack;
+
+public class Solution098 {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                // 在这里把根节点，左子节点推入栈；在循环的下一周期，把右子节点推入栈，以此达到换边的作用，因为下一句会再次切换回左子节点
+                // 所以这个栈就很重要，它储存的是根节点和他的所有左子节点以及左叶子节点对应的那个右子节点
+                // 这就使得在pop的时候，pre是左子节点，root就是根节点；pre变为根节点之后root又变成了右子节点，所以下面的判断条件不用变，很巧妙
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
