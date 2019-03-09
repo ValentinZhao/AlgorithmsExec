95694188a0072469fd1f4cd362b874a4e6c2afef
ValentinZhao <2264709714@qq.com>
2019-03-09 23:35:04 +0800

    add lc269

---
+++ src/leetcode/lc076/Solution076.java
@@ -0,0 +1,32 @@
+package lc076;
+
+public class Solution076 {
+    public String minWindow(String s, String t) {
+        int[] arr = new int[128];
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i)]++;
+        }
+
+        int counter = t.length(), left = 0, right = 0, window = Integer.MAX_VALUE, head = -1;
+        while (right < s.length()) {
+            char rc = s.charAt(right++);
+            if (arr[rc] > 0) {
+                counter--;
+            }
+            arr[rc]--;
+
+            while (counter == 0) {
+                if (right - left < window) {
+                    window = right - (head = left);
+                }
+
+                char lc = s.charAt(left++);
+                if (arr[lc] == 0) {
+                    counter++;
+                }
+                arr[lc]++;
+            }
+        }
+        return head == -1 ? "" : s.substring(head, head + window);
+    }
+}
