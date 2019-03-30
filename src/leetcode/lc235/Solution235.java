d5d2effe1d12b0ee744193ec4408f178d595807d
ValentinZhao <2264709714@qq.com>
2019-03-30 18:52:00 +0800

    add lc235

---
+++ src/leetcode/lc235/Solution235.java
@@ -0,0 +1,11 @@
+package lc235;
+
+import TreeNode.TreeNode;
+
+public class Solution235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+    }
+}
