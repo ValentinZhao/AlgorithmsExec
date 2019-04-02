613c448ff6ce5c109ae1a978d06a6f61a2066b78
ValentinZhao <2264709714@qq.com>
2019-04-02 11:21:15 +0800

    add lc145

---
+++ src/leetcode/lc145/Solution145.java
@@ -0,0 +1,65 @@
+package lc145;
+
+import TreeNode.TreeNode;
+
+import java.util.*;
+
+/**
+ * 这里有一个很完善的前中后序遍历迭代写法的总结
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
+ */
+
+// 后序遍历就是前序反过来
+public class Solution145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                list.add(0, p.val);
+                stack.push(p);
+                p = p.right;
+            } else {
+                TreeNode node = stack.pop();
+                p = node.left;
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                result.add(p.val);  // Add before going to children
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                p = node.right;
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                result.add(node.val);  // Add after all left children
+                p = node.right;
+            }
+        }
+        return result;
+    }
+}
