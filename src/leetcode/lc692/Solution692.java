542db13dbe712eb719864fe1ac89b158a223d25f
ValentinZhao <2264709714@qq.com>
2019-05-03 15:57:40 +0800

    add lc692

---
+++ src/leetcode/lc692/Solution692.java
@@ -0,0 +1,28 @@
+package lc692;
+
+import java.util.*;
+
+/**
+ * 使用小顶堆不使用大顶堆主要是为了O(nlogK)的时间复杂度
+ */
+public class Solution692 {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> res = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) map.put(word, map.get(word)+1);
+            else map.put(word, 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) pq.poll();
+        }
+        while (!pq.isEmpty()) {
+            res.add(0, pq.poll().getKey());
+        }
+        return res;
+    }
+}
