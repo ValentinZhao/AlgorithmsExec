aa150be3ecbcc624300b353c7ade52d844b726af
ValentinZhao <2264709714@qq.com>
2019-08-09 18:05:07 +0800

    2nd lc056

--- src/leetcode/lc056/Solution056.java
+++ src/leetcode/lc056/Solution056.java
@@ -1,5 +1,6 @@
 package lc056;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -41,4 +42,23 @@ class Interval {
       int end;
       Interval() { start = 0; end = 0; }
       Interval(int s, int e) { start = s; end = e; }
+}
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int[][] res = new int[intervals.length][2];
+        for (int i = 0; i < intervals.length; i++) {
+            if (end >= intervals[i][0]) {
+                end = intervals[i][1];
+            } else {
+                res[i] = new int[]{start, end};
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        return res;
+    }
 }
