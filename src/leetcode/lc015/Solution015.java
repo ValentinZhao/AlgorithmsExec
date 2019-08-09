81edb11288e96a6a39977b1e58012ce1c23c0460
ValentinZhao <2264709714@qq.com>
2019-08-09 17:45:27 +0800

    2nd lc015

--- src/leetcode/lc015/Solution015.java
+++ src/leetcode/lc015/Solution015.java
@@ -1,5 +1,6 @@
 package lc015;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,6 +17,28 @@ public class Solution015 {
                         res.add(Arrays.asList(nums[i], nums[start], nums[end]));
                         while (start < end && nums[start] == nums[start + 1]) start++;
                         while (start < end && nums[end] == nums[end - 1]) end--;
+                        start++;end--;
+                    } else if (nums[start] + nums[end] > sum) end--;
+                    else start++;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1, end = nums.length - 1, sum = 0 - nums[i];
+            if (i == 0 || nums[i] != nums[i-1]) {
+                while (start < end) {
+                    if (nums[start] + nums[end] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        while (start < end && nums[start] == nums[start+1]) start++;
+                        while (start < end && nums[end] == nums[end-1]) end--;
                         start++;end--;
                     } else if (nums[start] + nums[end] > sum) end--;
                     else start++;
