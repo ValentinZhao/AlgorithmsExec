6015117119ba6bcfb7e9cb80f08061fb6aa48580
ValentinZhao <2264709714@qq.com>
2019-08-14 16:23:58 +0800

    2nd lc101

--- src/leetcode/lc101/Solution101.java
+++ src/leetcode/lc101/Solution101.java
@@ -13,3 +13,15 @@ public class Solution101 {
         return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
 }
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || symmetricHelper(root.left, root.right);
+    }
+
+    private boolean symmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null || right == null) return left == right;
+        if (left.val != right.val) return false;
+        return symmetricHelper(left.left, right.right) && symmetricHelper(left.right, right.left);
+    }
+}
