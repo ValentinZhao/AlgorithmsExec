8c8d7c22c57d2da7578e58c8c48fd9d0cf5f4570
ValentinZhao <2264709714@qq.com>
2019-08-14 16:59:55 +0800

    2nd lc102

--- src/leetcode/lc102/Solution102.java
+++ src/leetcode/lc102/Solution102.java
@@ -28,3 +28,24 @@ public class Solution102 {
         return list;
     }
 }
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
