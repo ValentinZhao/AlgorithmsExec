4a25e5497350929814c9c4f86c071985968f6c6e
ValentinZhao <2264709714@qq.com>
2019-08-15 14:24:00 +0800

    2nd lc199

--- src/leetcode/lc199/Solution199.java
+++ src/leetcode/lc199/Solution199.java
@@ -30,3 +30,22 @@ public class Solution199 {
         return list;
     }
 }
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                if (size == 0) res.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+        }
+        return res;
+    }
+}
