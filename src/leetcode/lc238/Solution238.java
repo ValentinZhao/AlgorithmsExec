c0696da8301034d10896dfe67f095bbb49dcbce1
ValentinZhao <2264709714@qq.com>
2019-08-19 09:22:42 +0800

    2nd lc560

--- src/leetcode/lc238/Solution238.java
+++ src/leetcode/lc238/Solution238.java
@@ -29,7 +29,7 @@ class Solution {
         // Note: for the element at index '0', there are no elements to the left,
         // so the answer[0] would be 1
         answers[0] = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             // answer[i - 1] already contains the product of elements to the left of 'i - 1'
             // Simply multiplying it with nums[i - 1] would give the product of all
             // elements to the left of index 'i'
@@ -39,7 +39,7 @@ class Solution {
         // Note: for the element at index 'length - 1', there are no elements to the right,
         // so the R would be 1
         int r = 1;
-        for (int i = n-1; i >= 0 ; i++) {
+        for (int i = n-1; i >= 0 ; i--) {
             // For the index 'i', R would contain the
             // product of all elements to the right. We update R accordingly
             answers[i] *= r;
