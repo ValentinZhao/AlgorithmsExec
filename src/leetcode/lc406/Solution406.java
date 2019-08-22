e2be889b10ecad78da4a7359bde064677d4e3ad6
ValentinZhao <2264709714@qq.com>
2019-08-22 11:26:08 +0800

    2nd lc406

--- src/leetcode/lc406/Solution406.java
+++ src/leetcode/lc406/Solution406.java
@@ -28,3 +28,18 @@ public class Solution406 {
         return res;
     }
 }
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        int[][] res = new int[people.length][2];
+        for (int i = 0; i < people.length; i++) {
+            int pos = people[i][1];
+            for (int j = i; j > pos; j--) {
+                res[j] = res[j-1];
+            }
+            res[pos] = people[i];
+        }
+        return res;
+    }
+}
