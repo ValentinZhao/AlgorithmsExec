629f484f73d2bc613b59197f7035bf78f4cf5909
ValentinZhao <2264709714@qq.com>
2019-08-23 18:17:34 +0800

    2nd lc039

--- src/leetcode/lc039/Solution039.java
+++ src/leetcode/lc039/Solution039.java
@@ -24,3 +24,26 @@ public class Solution039 {
         }
     }
 }
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtracking(res, new ArrayList<>(), candidates, target, 0);
+        return res;
+    }
+
+    private void backtracking(List<List<Integer>> res, ArrayList<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) {
+            res.add(new ArrayList<>(tempList));
+            return;
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                tempList.add(candidates[i]);
+                backtracking(res, tempList, candidates, remain - candidates[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
