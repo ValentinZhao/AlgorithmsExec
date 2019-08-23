46b3b2c78b8c5af3c87e76ae2c0d595533043894
ValentinZhao <2264709714@qq.com>
2019-08-23 14:09:45 +0800

    2nd lc621

--- src/leetcode/src/Main.java
+++ src/leetcode/src/Main.java
@@ -3,7 +3,7 @@ package src;
 
 import TreeNode.TreeNode;
 import lc002.Solution002.ListNode;
-import lc317.Solution317;
+import lc621.Solution621;
 
 import java.util.Arrays;
 
@@ -11,21 +11,22 @@ public class Main {
     public static void main (String[] args) {
 //        TreeNode root = generateSampleTreeNode();
 //        Solution173.BSTIterator solution = new Solution173.BSTIterator(root);
-        Solution317 solution = new Solution317();
+        Solution621 solution = new Solution621();
 //        int[] data = new int[]{0,0,0,1,3,5,6,7,8,8};
 //        String data = "2*3-4*5";
-        int[][] matrix = new int[][]{{1,0,2,0,1},{0,0,0,0,0},{0,0,1,0,0}};
+//        int[][] matrix = new int[][]{{1,0,2,0,1},{0,0,0,0,0},{0,0,1,0,0}};
 //        int[][] matrix = new int[][]{{-1,3}};
 //        int [][] matrix = new int[][]{{0,0,0}, {0,1,0}, {1,1,1}};
 //        String[][] tickets = new String[][]{{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
 //        String[] str_arr = new String[]{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"};
+        char[] datas = new char[]{'A','A','A','B','B','B'};
 //        Arrays.sort(str_arr);
         int test_int = 3;
 //        ListNode l1 = generateSampleListNode();
 //        ListNode l2 = generateSampleListNode();
 //        ListNode res = solution.addTwoNumbers(l1, l2);
-        int result = solution.shortestDistance(matrix);
-        System.out.println("Input =====> " + Arrays.toString(matrix));
+        int result = solution.leastInterval(datas, 2);
+        System.out.println("Input =====> " + Arrays.toString(datas));
 //        System.out.println("Input =====> " + Arrays.toString(matrix));
         System.out.println("Output =====> " + result);
 //        System.out.println("=======INPUT=======");
