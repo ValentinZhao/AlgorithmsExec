6f98d6d177e6e4967d676c4c7baca596e6d79dd0
ValentinZhao <2264709714@qq.com>
2019-08-30 16:38:15 -0700

    2nd lc33

--- src/leetcode/lc033/Solution033.java
+++ src/leetcode/lc033/Solution033.java
@@ -60,3 +60,29 @@ public class Solution033 {
     }
 
 }
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = Math.round(((float)start + end) / 2);
+            if (nums[mid] < nums[start]) end = mid - 1;
+            else start = mid;
+        }
+        int n = nums.length;
+        int offset = (start + n) - (n - 1);
+        start = 0;
+        end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int midChange = (mid + offset) % n;
+            int value = nums[midChange];
+            if (target == value) return midChange;
+            else if (target > value) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
+    }
+}
