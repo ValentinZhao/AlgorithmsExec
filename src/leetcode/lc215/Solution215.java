346a2843bcaba2ea11e8f314a1934bef1b686b26
ValentinZhao <2264709714@qq.com>
2019-08-29 17:16:02 -0700

    2nd lc215

--- src/leetcode/lc215/Solution215.java
+++ src/leetcode/lc215/Solution215.java
@@ -45,3 +45,33 @@ public class Solution215 {
         }
     }
 }
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums.length == 0 || nums == null) return Integer.MAX_VALUE;
+        // 因为我们是从结尾处pivot开始，把比他小的放在左面，那么其实我们做完快速选择其实是选出了前k个最小的
+        // 那么我们就需要取numLength - k才是最大的k个
+        return findKthLargest(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int findKthLargest(int[] nums, int start, int end, int k) {
+        if (start > end) return Integer.MAX_VALUE;
+        int pivot = nums[end];
+        int left = start;
+        for (int i = start; i < end; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, left++, i); // Put numbers < pivot to pivot's left
+            }
+        }
+        swap(nums, left, end); // Finally, swap A[end] with A[left]
+        if (left == k) return nums[left];
+        else if (left < k) return findKthLargest(nums, left + 1, end, k);
+        else return findKthLargest(nums, start, left - 1, k);
+    }
+
+    private void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+}
