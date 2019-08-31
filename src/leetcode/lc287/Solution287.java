a7d35b37fdc92ede946a69efb09bbbc1a4d43c72
ValentinZhao <2264709714@qq.com>
2019-08-31 07:34:07 -0700

    2nd lc287

--- src/leetcode/lc287/Solution287.java
+++ src/leetcode/lc287/Solution287.java
@@ -27,3 +27,22 @@ public class Solution287 {
         return -1;
     }
 }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums.length > 0) {
+            int slow = nums[0], fast = nums[nums[0]];
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            fast = 0;
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return slow;
+        }
+        return -1;
+    }
+}
