389971e24fcc23698c73c281ea9bca2edf6f0af6
ValentinZhao <2264709714@qq.com>
2019-09-01 10:14:46 -0700

    2nd lc240

--- src/leetcode/lc240/Solution240.java
+++ src/leetcode/lc240/Solution240.java
@@ -31,3 +31,21 @@ public class Solution240 {
         return false;
     }
 }
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return false;
+        // 最重要的就是记得从**右上角**开始遍历
+        int row = 0, col = matrix[0].length-1;
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else if(target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
