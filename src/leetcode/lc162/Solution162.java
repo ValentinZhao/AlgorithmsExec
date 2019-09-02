65edfdda5425a965dc5592a61bb4b7726c30a095
ValentinZhao <2264709714@qq.com>
2019-09-02 11:55:06 -0700

    2nd lc162

--- src/leetcode/lc162/Solution162.java
+++ src/leetcode/lc162/Solution162.java
@@ -15,3 +15,15 @@ public class Solution162 {
         return lo;
     }
 }
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo = 0, hi = nums.length - 1, mid = 0;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[mid+1]) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+}
