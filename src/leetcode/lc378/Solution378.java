9796910134a4cad7a86509cc15f3680031e93d2e
ValentinZhao <2264709714@qq.com>
2019-09-02 22:23:24 -0700

    2nd lc378

--- src/leetcode/lc378/Solution378.java
+++ src/leetcode/lc378/Solution378.java
@@ -23,3 +23,21 @@ public class Solution378 {
         return lo;
     }
 }
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int lo = matrix[0][0], hi = matrix[matrix.length-1][matrix[0].length-1];
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0;
+            int col = matrix[0].length - 1;
+            for (int i = 0; i < matrix.length; i++) {
+                while (col >= 0 && matrix[i][col] > mid) col--;
+                count += (col + 1);
+            }
+            if (count < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
