bf67cb070267c357c10c7c06455a25750fb22bd4
ValentinZhao <2264709714@qq.com>
2019-09-05 11:41:11 -0700

    2nd lc690

--- src/leetcode/lc690/Solution690.java
+++ src/leetcode/lc690/Solution690.java
@@ -33,3 +33,30 @@ public class Solution690 {
         public List<Integer> subordinates;
     };
 }
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee e : employees) map.put(e.id, e);
+        return getImportanceDFS(map, id);
+    }
+
+    private int getImportanceDFS(Map<Integer, Employee> map, int id) {
+        Employee root = map.get(id);
+        int res = root.importance;
+        for (int e : root.subordinates) {
+            res += getImportanceDFS(map, e);
+        }
+        return res;
+    }
+
+    class Employee {
+        // It's the unique id of each node;
+        // unique id of this employee
+        public int id;
+        // the importance value of this employee
+        public int importance;
+        // the id of direct subordinates
+        public List<Integer> subordinates;
+    }
+}
