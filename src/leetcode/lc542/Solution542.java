76e21fd8aafe883e664c3232abd733a9c0fa94f3
ValentinZhao <2264709714@qq.com>
2019-09-06 11:49:39 -0700

    2nd lc542

--- src/leetcode/lc542/Solution542.java
+++ src/leetcode/lc542/Solution542.java
@@ -38,3 +38,31 @@ public class Solution542 {
     }
 
 }
+
+class Solution {
+    private static int[][] dirs = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+    public int[][] updateMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return null;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) queue.offer(new int[]{i, j});
+                else matrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int[] dir : dirs) {
+                int row = cur[0] + dir[0];
+                int col = cur[1] + dir[1];
+                if (row < 0 || row >= m || col < 0 || col >= n ||
+                    matrix[row][col] <= matrix[cur[0]][cur[1]] + 1) continue;
+                queue.offer(new int[]{row, col});
+                matrix[row][col] = matrix[cur[0]][cur[1]] + 1;
+            }
+        }
+        return matrix;
+    }
+}
