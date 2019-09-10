598ae6d62560ad89f09f2814e423517bffc2a47c
ValentinZhao <2264709714@qq.com>
2019-09-10 01:07:50 -0700

    add lc814

---
+++ src/leetcode/lc814/Solution814.java
@@ -0,0 +1,18 @@
+package lc814;
+
+import TreeNode.TreeNode;
+
+public class Solution814 {
+    public TreeNode pruneTree(TreeNode root) {
+        return containsOne(root) ? root : null;
+    }
+
+    private boolean containsOne(TreeNode root) {
+        if (root == null) return false;
+        boolean left = containsOne(root.left);
+        boolean right = containsOne(root.right);
+        if (!left) root.left = null;
+        if (!right) root.right = null;
+        return root.val == 1 || left || right;
+    }
+}
