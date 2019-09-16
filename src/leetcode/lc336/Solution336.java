2b1340add414b8361ab7f761d433ebb9b5ca6b21
ValentinZhao <2264709714@qq.com>
2019-09-15 23:12:11 -0700

    2nd lc336

--- src/leetcode/lc336/Solution336.java
+++ src/leetcode/lc336/Solution336.java
@@ -1,8 +1,6 @@
 package lc336;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution336 {
     private static class TrieNode {
@@ -73,6 +71,58 @@ public class Solution336 {
             if (word.charAt(i++) != word.charAt(j--)) return false;
         }
 
+        return true;
+    }
+}
+
+/**
+ * 简单说下这方法就是，找到一个点，可以让某个词分开，其中第一段是回文串，第二段的reverse可以在map中找到并且不等于当前遍历位置
+ * 那么就可以说，reverse串+第一段+第二段就构成了大回文串，这时候把reverse的位置写在前，当前word位置写在后即可；
+ * 那么同理我们也可以找，当第二段是回文串时，去找reverseStr1的位置，并把它拼在后面
+ */
+class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (words == null || words.length == 0) return res;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], i);
+        }
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i];
+            for (int j = 0; j <= currentWord.length(); j++) {
+                String str1 = currentWord.substring(0, j);
+                String str2 = currentWord.substring(j);
+                if (isPalindrome(str1)) {
+                    String reversedStr2 = new StringBuilder(str2).reverse().toString();
+                    if (map.containsKey(reversedStr2) && map.get(reversedStr2) != i && str1.length() != 0) {
+                        List<Integer> pair = new ArrayList<>();
+                        pair.add(map.get(reversedStr2));
+                        pair.add(i);
+                        res.add(pair);
+                    }
+                }
+
+                if (isPalindrome(str2)) {
+                    String reversedStr1 = new StringBuilder(str1).reverse().toString();
+                    if (map.containsKey(reversedStr1) && map.get(reversedStr1) != i) {
+                        List<Integer> pair = new ArrayList<>();
+                        pair.add(i);
+                        pair.add(map.get(reversedStr1));
+                        res.add(pair);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
         return true;
     }
 }
