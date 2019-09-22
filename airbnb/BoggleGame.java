494f1308ee64db99b572b1ba87e80ccadd7da84e
ValentinZhao <2264709714@qq.com>
2019-09-22 01:54:34 -0700

    add lc756

--- airbnb/BoggleGame.java
+++ airbnb/BoggleGame.java
@@ -1,3 +1,39 @@
-class Solution {
-    
+/**
+代码是基本的DFS，下面是一个基于Trie的例子
+https://huangwenjimmy.github.io/2016/11/17/airbnb-boggle-game.html
+ */
+public class Solution {
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
+    public boolean exist(char[][] board, String word) {
+        if (board==null || board.length==0) return false;
+        boolean visited[][] = new boolean[board.length][board[0].length];
+        for (int i=0; i < board.length; i++){
+            for (int j=0; j < board[0].length; j++){
+                if (search(word, 0, board, i, j, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean search(String word, int index, char[][] board, int i, int j, boolean[][] visited){
+        if (i<0 || j<0 || i==board.length || j==board[0].length || visited[i][j]==true) return false;
+        visited[i][j] = true;
+        boolean result = false;
+        if (board[i][j]==word.charAt(index)){
+            if (index == word.length()-1) return true;
+            //save the result here instead of just return the result, as we need to unset the visited matrix before return
+            result = search(word, index+1, board, i-1, j, visited) ||
+                            search(word, index+1, board, i+1, j, visited) ||
+                            search(word, index+1, board, i, j-1, visited) ||
+                            search(word, index+1, board, i, j+1, visited);
+        }
+        visited[i][j] = false;
+        return result;
+    }
 }
---
+++ airbnb/PyramidTransitionMatrix.java
@@ -0,0 +1,52 @@
+// 本题同 Pyramid Transition Matrix
+// https://leetcode.com/problems/pyramid-transition-matrix/
+public class Solution {
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        Map<String, List<String>> map = new HashMap<>();
+        // 构建一个graph
+        for (String s : allowed) {
+            String key = s.substring(0, 2);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s.substring(2));
+        }
+        
+        return dfs(bottom, map);
+    }
+
+    private boolean dfs(String bottom, Map<String, List<String>> map) {
+        if (bottom.length() == 1) return true;
+        // 检查是否当前row的任意两个相邻的元素是否存在topping，如果有任何不存在的组合，直接返回错误，毕竟这对就无法继续向上搭建了
+        for (int i = 0; i < bottom.length() - 1; i++) {
+            if (!map.containsKey(bottom.substring(i, i+2))) return false;
+        }
+        
+        List<String> list = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        
+        // 利用回溯，组合出所有topping的组合，毕竟每两个的topping可能有多个
+        // 比如底层是ABCD，然后映射关系有ABC, ABG, CDE, CDF
+        // 那么topping level可能就有 CE, CF, GE, GF这样的组合
+        // 那么这不就是backtracking嘛
+        backtracking(bottom, builder, list, map, 0);
+        
+        for (String s : list) {
+            // 这里的s就是一层新的toppings，它们即将作为新的bottom继续向上DFS了
+            if (dfs(s, map)) return true;
+        }
+        return false;
+    }
+
+    private void backtracking(String bottom, StringBuilder builder, List<String> list, Map<String, List<String>> map, int index) {
+        if (index == bottom.length() - 1) {
+            list.add(builder.toString());
+            return;
+        }
+        
+        // 这个迭代器中的s，就是一个可行的toppping字母，用来组合的
+        for (String s : map.get(bottom.substring(index, index+2))) {
+            builder.append(s);
+            backtracking(bottom, builder, list, map, index + 1);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+}
