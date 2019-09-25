b586b0484fdc7697fbe044442eca83af8f312389
ValentinZhao <2264709714@qq.com>
2019-09-25 15:24:52 -0700

    2nd lc673

--- src/leetcode/lc673/Solution673.java
+++ src/leetcode/lc673/Solution673.java
@@ -43,3 +43,35 @@ public class Solution673 {
         return result;
     }
 }
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int maxLen = 0;\int res = 0;
+        int[] len = new int[n], cnt = new int[n];
+        for (int i = 0; i < n; i++) {
+            len[i] = cnt[i] = 1;
+            for (int j = 0; j < i; j++) {
+                // 存在一个递增的话
+                if (nums[i] > nums[j]) {
+                    // 如果在i之前的某一位，最长递增长度只比i短一位，那么直接把cnt[i]加上所有cnt[j]
+                    // 因为j一定是比i小的，最大的j就正好是i-1，那么LIS的长度只差一位，说明这正好是len[i]的一部分
+                    if (len[i] == len[j] + 1) cnt[i] += cnt[j];
+                    // 这时候我们发现原来记录的len[i]还没有他的子集遍历中发现的LIS更长，那么我们就直接更新为这个值
+                    else if (len[i] < len[j] + 1) {
+                        len[i] = len[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+                }
+            }
+            // maxLen记录的是上一位的最长LIS，如果这一步的LIS还是一样的
+            // 说明这一位的LIS个数要加上历史所有个数加上目前cnt
+            if (maxLen == len[i]) res += cnt[i];
+            else if (maxLen < len[i]) {
+                maxLen = len[i];
+                res = cnt[i];
+            }
+        }
+        return res;
+    }
+}
