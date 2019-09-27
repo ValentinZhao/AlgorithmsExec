e348488c95b86e9f9b08030bba11a8a1deb37b2e
ValentinZhao <2264709714@qq.com>
2019-09-27 00:36:28 -0700

    2nd lc785

--- src/leetcode/lc785/Solution785.java
+++ src/leetcode/lc785/Solution785.java
@@ -29,3 +29,37 @@ public class Solution785 {
         return true;
     }
 }
+
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];
+        for (int i = 0; i < n; i++) {
+
+            // 亮点在于，未着色用0，已着色用1，-1
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        // 已经着色了
+        if (colors[node] != 0) {
+            // color是上个节点的颜色，colors[node]是现在这个节点的颜色
+            return colors[node] == color;
+        }
+
+        colors[node] = color;
+        for (int neighbor : graph[node]) {
+            // 给下个节点染不一样的颜色
+            if (!validColor(graph, colors, -color, neighbor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
