6acfd74477916440046484ca50b50b53363cd089
ValentinZhao <2264709714@qq.com>
2019-10-01 17:10:45 -0700

    add lc029

---
+++ src/leetcode/lc029/Solution029.java
@@ -0,0 +1,24 @@
+package lc029;
+
+// 查看一下handbook有除法的整体运算法则
+public class Solution029 {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0 || divisor == -1 && dividend == Integer.MIN_VALUE) return Integer.MAX_VALUE;
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1; // 异或，有一个是negative，就是负的
+
+        long dvd = Math.abs(dividend);
+        long dvs = Math.abs(divisor);
+
+        int res = 0;
+        while (dvs <= dvd) {
+            long temp = dvs, mul = 1;
+            while (dvd >= temp<<1) {
+                temp <<= 1;
+                mul <<= 1;
+            }
+            dvd -= temp;
+            res += mul;
+        }
+        return sign > 0 ? res : -res;
+    }
+}
