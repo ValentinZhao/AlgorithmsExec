a7cb8456e878d3f8654926950ffe46630ffe7dd8
ValentinZhao <2264709714@qq.com>
2019-10-05 19:22:49 -0700

    add lc025

--- src/leetcode/lc025/Solution025.java
+++ src/leetcode/lc025/Solution025.java
@@ -23,3 +23,29 @@ public class Solution025 {
         return head;
     }
 }
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+        // 存在k个nodes
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+            // 这时候，如果后面还有大于k个nodes，这个curr就是下个k nodes
+            // 翻转过后的头，比如1 <- 2 <- 3中的1，他正好是当前K nodes的结尾的后一个node
+            // 如果后面不足K个，他也是头，只不过没翻转
+            while (count-- > 0) {
+                ListNode next = head.next;
+                head.next = curr;
+                curr = head;
+                head = next;
+            }
+            head = curr;
+        }
+        return head;
+    }
+}
