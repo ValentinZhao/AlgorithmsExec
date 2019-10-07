6da6e9567750300a565a4d61390b0ea44f10f6a0
ValentinZhao <2264709714@qq.com>
2019-10-07 16:15:43 -0700

    add lc430

--- src/leetcode/lc430/Solution430.java
+++ src/leetcode/lc430/Solution430.java
@@ -27,20 +27,46 @@ public class Solution430 {
         }
         return head;
     }
+}
 
-    class Node {
-        public int val;
-        public Node prev;
-        public Node next;
-        public Node child;
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
 
-        public Node() {}
+    public Node() {}
 
-        public Node(int _val,Node _prev,Node _next,Node _child) {
-            val = _val;
-            prev = _prev;
-            next = _next;
-            child = _child;
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+}
+
+class Solution {
+    public Node flatten(Node head) {
+        // 如果不把head这个指针留下来，你是无法返回正确head的
+        // 毕竟链表也不能往回走
+        Node cursor = head;
+        while (cursor != null) {
+            Node next = cursor.next;
+
+            if (cursor.child != null) {
+                Node child = flatten(cursor.child);
+                cursor.next = child;
+                child.prev = cursor;
+                while (cursor.next != null) cursor = cursor.next;
+                if (next != null) {
+                    cursor.next = next;
+                    next.prev = cursor;
+                }
+            }
+
+            cursor = cursor.next;
         }
-    };
+
+        return head;
+    }
 }
