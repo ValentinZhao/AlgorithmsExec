fa3ebb4b0e9ef9b9503ff05aba3703bcde8228f8
ValentinZhao <2264709714@qq.com>
2019-10-07 21:52:49 -0700

    add lc977

---
+++ src/leetcode/lc977/Solution977.java
@@ -0,0 +1,11 @@
+public class Solution977 {
+    public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int[] ans = new int[N];
+        for (int i = 0; i < N; ++i)
+            ans[i] = A[i] * A[i];
+
+        Arrays.sort(ans);
+        return ans;
+    }
+}
