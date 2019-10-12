084f6e1800d818fe6ecf48ff5c06943ac0326488
ValentinZhao <2264709714@qq.com>
2019-10-12 15:46:38 -0700

    add sum-k explanation in lc560

--- src/leetcode/lc560/Solution560.java
+++ src/leetcode/lc560/Solution560.java
@@ -16,6 +16,9 @@ public class Solution560 {
             if (map.containsKey(sum - k)) { // sum - k能在map中找到的话，说明存在着这样的sum1，使得sum-sum1=k成立
                 // 毕竟sum相当于sum[0,j]，sum1相当于sum[0,i-1]，那么sum[i,j]就是sum-sum1咯
                 // 这时候的结果就是取出来的一段continuous sub-array
+                // 至于为什么取这里的count，原因是这个sum是从前往后遍历得到的，数组元素可能有正有负
+                // 那么得到这个sum-k的组合可能有多个，比如我们需要的sum-k是2，一个数组是[1,1,-2,2]
+                // 这时候前两个和前四个都是可以的，那么sum-k的value就是2，要一起计入count中
                 count += map.get(sum - k);
             }
             map.put(sum, map.getOrDefault(sum, 0) + 1);
