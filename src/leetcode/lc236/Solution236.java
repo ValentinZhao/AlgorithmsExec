18079de45d8049f451efdcc49661dd06c09f8728
ValentinZhao <2264709714@qq.com>
2019-10-13 14:44:32 -0700

    2nd lc238

--- src/leetcode/lc236/Solution236.java
+++ src/leetcode/lc236/Solution236.java
@@ -16,3 +16,15 @@ public class Solution236 {
         return left == null ? right : left; // 这样的情况是p和q都在子树的左边或右边，这样就返回不为null的那棵子树即可
     }
 }
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null && right == null) return null;
+        if (left != null && right != null) return root;
+        return left == null ? right : left;
+    }
+}
