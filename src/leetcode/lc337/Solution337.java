cf3b8a1d0f943b356b58a0fd20fa23acb728aad8
ValentinZhao <2264709714@qq.com>
2019-11-11 16:29:21 -0800

    2nd lc337

--- src/leetcode/lc337/Solution337.java
+++ src/leetcode/lc337/Solution337.java
@@ -22,3 +22,24 @@ public class Solution337 {
         return res;
     }
 }
+
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robDFS(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] robDFS(TreeNode root) {
+        int[] res = new int[2];
+        if (root == null) return res;
+        int[] left = robDFS(root.left);
+        int[] right = robDFS(root.right);
+
+        res[0] = root.val + left[1] + right[1];
+        // res[1]是不rob当前节点时，最大的收益，那么由于此点不rob，它的子节点可以选择rob或者依然也不rob，毕竟确实存在一种情况说第三个点比前两个点大得多
+        // 那这时候应该是连续两次不rob转而rob第三家
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return res;
+    }
+}
