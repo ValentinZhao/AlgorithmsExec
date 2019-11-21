adb96209d821f9e2e67bc9985470cdc27b664a8f
ValentinZhao <2264709714@qq.com>
2019-11-20 20:58:37 -0800

    add lc991

--- src/leetcode/lc991/Solution991.java
+++ src/leetcode/lc991/Solution991.java
@@ -1,5 +1,15 @@
+package lc991;
+
 public class Solution991 {
     public int brokenCalc(int X, int Y) {
-        
+        int ans = 0;
+
+        while (X < Y) {
+            ans++;
+            if (Y % 2 == 1) Y++; // 奇数直接除2会出现个数偏移，要加到高位
+            else Y /= 2;
+        }
+
+        return ans + X - Y;
     }
 }
