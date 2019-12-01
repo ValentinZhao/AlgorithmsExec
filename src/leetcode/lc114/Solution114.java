fe213678e5cc75075afa56c4043becab8ef089e2
ValentinZhao <2264709714@qq.com>
2019-11-30 21:08:23 -0800

    2nd lc114

--- src/leetcode/lc114/Solution114.java
+++ src/leetcode/lc114/Solution114.java
@@ -30,7 +30,7 @@ import TreeNode.TreeNode;
  *
  *   所以总结起来就是非常经典的DFS思想，把右子树的东西给到左子树的最右叶子节点，拼完之后，左子树也会被拼到上面根节点的right，这样形成flatten
  */
-public class Solution114 {
+public class  Solution114 {
     private TreeNode prev = null;
 
     public void flatten(TreeNode root) {
@@ -40,5 +40,52 @@ public class Solution114 {
         root.right = prev;
         root.left = null;
         prev = root;
+    }
+}
+
+/**
+ * 这道题要求把二叉树展开成链表，根据展开后形成的链表的顺序分析出是使用先序遍历，
+ * 那么只要是数的遍历就有递归和非递归的两种方法来求解，这里我们也用两种方法来求解。
+ * 首先来看递归版本的，思路是先利用 DFS 的思路找到最左子节点，然后回到其父节点，把其父节点和右子节点断开，
+ * 将原左子结点连上父节点的右子节点上，然后再把原右子节点连到新右子节点的右子节点上，然后再回到上一父节点做相同操作。
+ *
+ *      1
+ *     / \
+ *    2   5
+ *   / \   \
+ *  3   4   6
+ *
+ *      1
+ *     / \
+ *    2   5
+ *     \   \
+ *      3   6
+ *       \
+ *        4
+ *
+ *    1
+ *     \
+ *      2
+ *       \
+ *        3
+ *         \
+ *          4
+ *           \
+ *            5
+ *             \
+ *              6
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        if (root.left != null) flatten(root.left);
+        if (root.right != null) flatten(root.right);
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = tmp;
     }
 }
