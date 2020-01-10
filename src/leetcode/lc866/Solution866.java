0f10c042cdc1580d97a86d600947def858760e8c
ValentinZhao <2264709714@qq.com>
2020-01-10 12:23:16 -0800

    modify lc866

--- src/leetcode/lc866/Solution866.java
+++ src/leetcode/lc866/Solution866.java
@@ -3,6 +3,7 @@ package lc866;
 public class Solution866 {
     public int primePalindrome(int N) {
         if (8 <= N && N <= 11) return 11;
+        // 这里是5位数，因为我们是用substring(1)来取数，会切掉一位，这样最多就是4位拼4位是8位，题目限制最多就是10^8
         for (int x = 1; x < 100000; x++) {
             String s = Integer.toString(x), r = new StringBuilder(s).reverse().toString();
             int y = Integer.parseInt(s + r.substring(1));
