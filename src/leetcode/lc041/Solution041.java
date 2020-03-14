da33b102afae2ad9f65f7ff75c7fb65085711c5a
ValentinZhao <2264709714@qq.com>
2020-03-13 17:30:20 -0700

    2nd 31 41

--- src/leetcode/lc041/Solution041.java
+++ src/leetcode/lc041/Solution041.java
@@ -59,6 +59,7 @@ class Solution1 {
         int i = 0;
         while(i < A.length){
             if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            // 也就是说本来有序的数组应该是A[i]-1 = i的，那么如果下面等式不成立，我们把正确的数字换过来
             else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
             else i++;
         }
